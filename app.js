b23682ddd9573ee26be54f88d355cfdc813d91c5
Oğuz Özsaygın <oguzozsaygin@sabanciuniv.edu>
2020-06-18 20:54:28 +0300

    Remove unnecesary code sections and establish mongoose integration

--- app.js
+++ app.js
@@ -2,9 +2,26 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var multer = require('multer');
+var mongoose = require('mongoose');
+
+/* mongoose */
+var MONGO_URI = 'mongodb://localhost:27017/sudosk'
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).
+    catch(error => handleError(error));
+
+mongoose.connection.on('connecting', msg => {
+    console.log(`connecting to MongoDB server at ${MONGO_URI}`)
+})
+mongoose.connection.on('error', msg => {
+    console.log(`error occured: ${msg}`);
+});
+mongoose.connection.on('connected', err => {
+    console.log(`connected to ${MONGO_URI}`)
+})
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var climbPathRouter = require('./routes/climbPathRoute');
 
 var app = express();
 
@@ -15,6 +32,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/climbPath/', climbPathRouter);
 
 module.exports = app;
